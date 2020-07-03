94287962b3e5fee0957c68cdbb90b2482feefa22
merch <58620696+a-discount@users.noreply.github.com>
2020-07-03 10:57:44 -0700

    v0.1

---
+++ credits.js
@@ -0,0 +1,4 @@
+document.getElementById("myCredits").addEventListener("click", myFunction);
+    function myFunction() {
+  window.open("https://youtube.com/forgamingc?sub_confirmation=1/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=675,height=400");
+}
